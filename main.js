59d14f35b42dadb865acd70adc98a8144c89df8b
arunclenton <arunclenten@gmail.com>
2022-08-07 18:35:35 +0530

    add file

---
+++ main.js
@@ -0,0 +1,69 @@
+// var x = 'aeiouAEIOU'
+// var y = "arun clenton";
+// var sum='';
+// for(i=0; i<x.length;i++)
+//     for(j=0;j<y.length;j++)
+//     if(x[i]==y[j])
+//     {
+//         sum++
+//     }
+//     console.log(sum);
+
+
+
+// // =========================================================
+
+
+// var a = "arunclenten@mail.com"
+// var x = '';
+// var y = '';
+// for(i=0;i<a.length;i++)
+//     // console.log(i);
+
+
+// if(a[i]=="@")
+// // console.log(a[i]);
+// {
+//     for(j=0;j<i;j++)
+//     // console.log(j);
+
+//     {
+//         if(a[j]>=0||a[j]<=0)
+//         {
+//             x=x+a[j];
+           
+//         }
+//         else{
+//             y=y+a[j];
+//         }
+//     }
+//     // console.log(x);
+//     console.log(y);
+   
+// }
+
+
+// ==================================================
+
+// function myFunction()
+// {
+//   var myString = document.getElementById("string").innerHTML;
+//   var myWord = document.getElementById("text1").value;
+//   var myPattern = new RegExp('.'+myWord,'g');
+
+//   var myResult = myString.split(" ");
+//   console.log(myPattern)
+
+// //   for(var i=0; i<myResult.length; i++) {
+// //     var result = myResult[i].match(myPattern);
+// //     if(result) break;
+// //   }
+
+// //   document.getElementById("display").innerHTML = myResult;
+
+//  }
+
+
+var a = [aaaaaaaaaaaa];
+sum=0;
+for()
